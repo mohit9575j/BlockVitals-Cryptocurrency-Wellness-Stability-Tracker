f634dedf09d293799dd44ff7c5b543d98340baa8
Mohit <joshimohit8130@gmail.com>
2025-07-22 09:57:57 +0530

    Update auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -3,7 +3,7 @@ import { register, login } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+router.post('/register', register);
+router.post('/login', login);
 
 export default router;
